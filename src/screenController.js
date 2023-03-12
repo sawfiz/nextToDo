c8f193ba70d8c9e51b2ce486c074fb035bbccd1f
sawfiz <dashu.up@gmail.com>
2023-03-12 13:01:37 +0800

    Make Inbox and projects clickable - Added Inbox event listner - Added event listener for each project - Clicking on each of the above displays their associated tasks

--- src/screenController.js
+++ src/screenController.js
@@ -7,15 +7,21 @@ const screenController = () => {
   const projects = Projects();
 
   let activeProjectName = 'Inbox';
+  let activeProject = projects.inbox;
 
-  const updateTasksDisplay = (activeProject) => {
+  const updateTasksDisplay = (project) => {
     const tasksListEl = document.querySelector('.tasks-list');
     tasksListEl.innerHTML = '';
-    activeProject.tasks.forEach((task) => {
+    activeProject.tasks.forEach((task, index) => {
       const taskEl = createElement('div', ['task'], {}, '');
       const taskStatusEl = createElement('div', [], {}, task.status);
       taskEl.appendChild(taskStatusEl);
-      const taskTitleEl = createElement('div', [], {}, task.title);
+      const taskTitleEl = createElement(
+        'div',
+        [],
+        {},
+        `${task.title} ${index}`
+      );
       taskEl.appendChild(taskTitleEl);
       const taskDescriptionEl = createElement('div', [], {}, task.description);
       taskEl.appendChild(taskDescriptionEl);
@@ -25,6 +31,7 @@ const screenController = () => {
     });
   };
 
+  // Event listener for the add task button
   const addTaskBtn = document.querySelector('.add-task');
   addTaskBtn.addEventListener('click', () => {
     const task = Task(
@@ -47,17 +54,56 @@ const screenController = () => {
   const updateProjectsDisplay = () => {
     const projectsListEl = document.querySelector('.projects-list');
     projectsListEl.innerHTML = '';
-    projects.projects.forEach((project) => {
-      const projectEl = createElement('div', [], {}, project.name);
+    projects.projects.forEach((project, index) => {
+      const projectEl = createElement(
+        'div',
+        [],
+        { style: 'cursor: pointer', 'data-id': index },
+        `${project.name} ${index}`
+      );
+      // projectEl.dataset.id = index;
       projectsListEl.appendChild(projectEl);
     });
   };
 
+  // Event listener for the add project button
   const addProjectBtn = document.querySelector('.add-project');
   addProjectBtn.addEventListener('click', () => {
-    projects.addProject('Shopping');
-    activeProjectName = 'Shopping';
+    const newProjectName = prompt('Enter a new project name?');
+    projects.addProject(newProjectName);
+    activeProjectName = newProjectName;
+    activeProject = projects.projects[projects.projects.length - 1];
+    console.log(
+      '🚀 ~ file: screenController.js:61 ~ addProjectBtn.addEventListener ~ activeProject:',
+      activeProject
+    );
     updateProjectsDisplay();
+    updateTasksDisplay(activeProject);
+  });
+
+  // Event listener for Inbox
+  const inboxEl = document.querySelector('.inbox');
+  inboxEl.addEventListener('click', () => {
+    activeProject = projects.inbox;
+    activeProjectName = activeProject.name;
+    updateTasksDisplay(activeProject);
+  });
+
+  // Event listener for Projects List
+  const projectsEl = document.querySelector('.projects-list');
+  projectsEl.addEventListener('click', (e) => {
+    console.log(
+      '🚀 ~ file: screenController.js:92 ~ projectsEl.addEventListener ~ e:',
+      e
+    );
+    const index = e.target.dataset.id;
+    console.log(
+      '🚀 ~ file: screenController.js:87 ~ projectsEl.addEventListener ~ index:',
+      index
+    );
+    activeProject = projects.projects[index];
+    activeProjectName = activeProject.name;
+    updateTasksDisplay(activeProject);
   });
 };
 
